709afa0cb4366e07f1c2199400ae215acd2ae76d
vishalicious213 <vishalicious213@gmail.com>
2020-05-16 13:06:54 -0400

    login page fields set up and centered

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,10 +7,24 @@ const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <section className='login-page'>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form className='login-form'>
+        <input 
+          type='text' 
+          name='username' 
+          placeholder='Username'
+          value={username}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password'
+          value={password}
+        />
+        <button>Login</button>
+      </form>
+    </section>
   );
 };
 
