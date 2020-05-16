43b700aad687306285f52f9b46b4139b7b09b18f
vishalicious213 <vishalicious213@gmail.com>
2020-05-16 12:50:36 -0400

    initial commit. installed axios. set up username & password state in Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
